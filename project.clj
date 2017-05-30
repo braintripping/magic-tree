541d76563fabac11d7d787547008181c0f5d475c
Matthew Huebert <no-email@gmail.com>
2017-05-30 16:20:23 +0200

    lein-release plugin: preparing 0.0.4 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject magic-tree "0.0.4-SNAPSHOT"
+(defproject magic-tree "0.0.4"
   :description "Clojure(Script) Source Tool"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
