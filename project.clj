1373bf85918e40fdd42d4b38c835de4472cbb94e
Matthew Huebert <no-email@gmail.com>
2017-07-11 14:09:05 +0200

    Version bump, api changes

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject magic-tree "0.0.6-SNAPSHOT"
+(defproject magic-tree "0.0.7-SNAPSHOT"
   :description "Clojure(Script) Source Tool"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
