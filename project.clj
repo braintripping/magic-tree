625c6f6e07094d4b6ef4550904e39276c26a2a08
Matthew Huebert <no-email@gmail.com>
2017-07-26 15:11:02 +0200

    lein-release plugin: preparing 0.0.8 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject magic-tree "0.0.8-SNAPSHOT"
+(defproject magic-tree "0.0.8"
   :description "Clojure(Script) Source Tool"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
