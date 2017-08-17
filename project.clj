5dad0804812848afb8cc03c57a9708b1a27809c1
Matthew Huebert <no-email@gmail.com>
2017-08-17 16:06:10 +0200

    lein-release plugin: bumped version from 0.0.11 to 0.0.12-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject magic-tree "0.0.11"
+(defproject magic-tree "0.0.12-SNAPSHOT"
   :description "Clojure(Script) Source Tool"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
